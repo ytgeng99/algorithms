fa5b59c15716f7b2710bb8be772727d6f250d786
ytgeng99 <eric.genng@gmail.com>
2018-04-11 10:50:51 -0700

    add partitionarrat

---
+++ array/PartitionArray.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /*
+     * @param nums: The integer array you should partition
+     * @param k: An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+
+        if (nums == null || nums.length == 0) {
+        	return 0;
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+        	while(left <= right && nums[left] < k){
+        		left++;
+        	}
+        	while (left <= right && nums[right] >= k) {
+        		right--;
+        	}
+        	if ( left <= right) {
+    			int temp = nums[left];
+    			nums[left] = nums[right];
+    			nums[right] = temp;
+        	
+        		left++;
+        		right--;
+        	}
+        	
+        }
+        return left;
+    }
+}
