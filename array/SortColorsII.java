5ac9cd278040a09aa1101045f158eb138cd149ac
ytgeng99 <eric.genng@gmail.com>
2018-05-30 22:43:59 -0700

    sort colors II

---
+++ array/SortColorsII.java
@@ -0,0 +1,15 @@
+public class Solution {
+    /*
+     * @param colors: A list of integer
+     * @param k: An integer
+     * @return: nothing
+     */
+    public void sortColors2(int[] colors, int k) {
+        // write your code here
+        if (colors == null || colors.length < 4) {
+        	return;
+        }
+
+        
+    }
+}
